7134113ddce31f5dbfb9842951590bc72610b4a7
geirgardarsson <geg42@hi.is>
2018-10-30 13:14:21 +0000

    Added tempo to midi-json

--- Assets/Scripts/Python/midireader.py
+++ Assets/Scripts/Python/midireader.py
@@ -1,5 +1,7 @@
 import midi
+import json
 import argparse
+from collections import defaultdict
 
 
 parser = argparse.ArgumentParser()
@@ -10,15 +12,57 @@ args = parser.parse_args()
 song = midi.read_midifile(args.midi_file)
 song.make_ticks_abs()
 
+
 tracks = []
 
+
 for track in song:
-    notes = [note for note in track if note.name == 'Note On']
-    pitch = [note.pitch for note in notes]
-    tracks += [pitch]
 
-print(song[1])
+    notes = []
+    open_notes = defaultdict(int)
 
-#print(tracks)
-#print(len(tracks))
+    for note in track:
+
+        if note.name == 'Note On':
+            open_notes[note.pitch] = note.tick
+
+        elif note.name == 'Note Off':
+            notes.append({
+                'pitch': note.pitch,
+                'start': open_notes[note.pitch],
+                'end': note.tick
+            })
+
+            open_notes.pop(note.pitch)
+
+        else:
+            pass
+
+    tracks.append(notes)
+
+
+json_song = {
+    'name': args.midi_file.split('.')[0],
+    'tracks': tracks
+}
+
+'''
+60 bpm = 1 beat per second = 1000000 microseconds per beat
+
+Tempo events are expressed in microseconds, so your event
+should have a value of 1 million. But the data array uses
+8-bit words, so this number gets split up by powers of 256:
+
+1000000 = (15 * 256^2) + (66 * 256) + (64) = [15, 66, 64]
+'''
+
+for info in song[0]:
+    if info.name == "Set Tempo":
+        json_song["tempo"] = info.data
+
+
+
+with open(args.midi_file.split('.')[0] + '.json', "w") as outfile:
+    json.dump(json_song, outfile)
+
 
